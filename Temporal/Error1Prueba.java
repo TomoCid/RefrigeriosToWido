9158aa56f377841d50c39ce3ed00895bfef6c044
Brendan Rubilar <82827268+BrendanRubilar@users.noreply.github.com>
2022-10-18 18:33:00 -0300

    Subiendo Updates

---
+++ Temporal/Error1Prueba.java
@@ -0,0 +1,9 @@
+package Temporal;
+
+public class Error1Prueba extends Exception{
+
+    public Error1Prueba(String mensajeDeError){
+        super(mensajeDeError);
+    }
+
+}
--- Temporal/MainTemporal.java
+++ Temporal/MainTemporal.java
@@ -1,63 +1,15 @@
 package Temporal;
 
-public class A {
+public class MainTemporal {
 
     public static void main(String[] args) {
-        Expendedor soda = new Expendedor(1);
-        Moneda x = new Moneda();
-        Bebida xoda = soda.comprarBebida(x, 1);
-        if(xoda!=null){
-        System.out.println(xoda.beber()+xoda.getSerie());
-        }else{
-            System.out.println("No hay colas");
-        }
-        System.out.println(xoda.beber());
-    }
-}
+
+        //W. Testeando como funciona el llamado entre clases...
+        Moneda1000 moneda1 = new Moneda1000();
+        Expendedor expendedor1 = new Expendedor(2,200);
+        Comprador JuantitoCopoDeNieve = new Comprador(null, 0, expendedor1);
 
 
-class Sprite extends Bebida {
 
-    public Sprite(int s) {
-        super(s);
-    }
-
-    public String beber() {
-        String x = new String("sprite");
-        return super.beber() + x;
-    }
-}
-
-class CocaCola extends Bebida {
-
-    public CocaCola(int d) {
-        super(d);
-    }
-
-    @Override
-    public String beber() {
-        String w = new String("cocacola");
-        return super.beber() + w;
-    }
-}
-
-class Deposito {
-
-    private ArrayList<Bebida> al;
-
-    public Deposito() {
-        al = new ArrayList();
-    }
-
-    public void addBebida(Bebida a) {
-        al.add(a);
-    }
-
-    public Bebida getBebida() {
-        if (al.size() != 0) {
-            return al.remove(0);
-        } else {
-            return null;
-        }
     }
 }
